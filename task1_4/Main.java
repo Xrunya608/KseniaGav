5e9c3a1b8d1120b4a0ddcaa21ed0bea3160d9ebb
Xrunya608 <66119604+Xrunya608@users.noreply.github.com>
2020-06-02 16:31:34 +0300

    Create Main.java

---
+++ task1_4/Main.java
@@ -0,0 +1,24 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
+
+package com.company;
+
+class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        int a,b;
+        a = 25;
+        b = 25;
+        int mult = a*b;
+        System.out.println("25 * 25 = "+mult);
+    }
+}
