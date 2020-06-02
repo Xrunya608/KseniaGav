70ecd8a9408396bd94272e4c2065a4f821a4fb3d
Xrunya608 <66119604+Xrunya608@users.noreply.github.com>
2020-06-03 00:23:08 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -14,20 +14,23 @@ public class Main {
     }
 }
 
-
 package com.company;
 
 import java.util.Scanner;
 
 public class Main {
+    public static String TF = "(9 / 5) * TC + 32";
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
         int a;
         float num1 = 9;
         float num2 = 5;
-        float num3= 32;
-        System.out.print("Enter value Celcium: ");
-        a = scan.nextInt();
-        System.out.println("Значеник температуры по °F: "+((num1/num2) * a + num3));
+        float num3 = 32;
+        System.out.print("Enter value in Celcium: ");
+        a = scanner.nextInt();
+        System.out.println("Value in Farenheit: "+((num1/num2)*a + num3));
     }
+
 }
